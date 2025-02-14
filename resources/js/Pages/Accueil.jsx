6501bc2cc42e47cda12356c5f1d0de0d7fd75646
Antoine Auger <Ardyn142@gmail.com>
2025-02-14 00:01:13 -0500

    Clean Code + Fonts

---
+++ resources/js/Pages/Accueil.jsx
@@ -0,0 +1,12 @@
+import { Head } from "@inertiajs/react";
+
+export default function Accueil() {
+
+    return(
+        <>
+            <Head title="Accueil" />
+
+            <p>Hello, World</p>
+        </>
+    )
+}
