e553dc8784474bb3fc52a2bdcf95f53e335613dd
Antoine Auger <Ardyn142@gmail.com>
2025-03-14 00:14:05 -0400

    Navigation Bar Tweak

--- resources/js/Components/UI/PaginationBar.jsx
+++ resources/js/Components/UI/PaginationBar.jsx
@@ -1,6 +1,10 @@
-import { Link } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
 
 export default function PaginationBar({ links, params = null }) {
+
+    const { url } = usePage()
+    const searchParams = new URLSearchParams(new URL(window.location.origin + url).search)
+    const pageNo = parseInt(searchParams.get("page") || 1, 10)
 
     function buildUrl(link) {
         if (!params) return link
@@ -18,19 +22,64 @@ export default function PaginationBar({ links, params = null }) {
         return url.toString()
     }
 
+    function formatLinks() {
+        if (links.length < 13) return links
+
+        let paginatedLinks = [];
+        const isInDefault = pageNo < 3 || pageNo > links.length - 3
+
+        const firstLinks = links.slice(1, 3)
+        const lastLinks = links.slice(-3, -1)
+
+        const leftMidLinks = isInDefault
+            ? links.slice(3, 5)
+            : pageNo - 2 < 3
+                ? links.slice(3, pageNo)
+                : links.slice(pageNo - 2, pageNo)
+
+        const rightMidLinks = isInDefault
+            ? links.slice(-5, -3)
+            : pageNo + 3 > links.length - 3
+                ? links.slice(pageNo + 1, -3)
+                : links.slice(pageNo + 1, pageNo + 3)
+
+        paginatedLinks.push(links[0])
+        paginatedLinks.push(...firstLinks)
+
+        if (leftMidLinks.length > 0 && (parseInt(leftMidLinks[0].label) != 3)) paginatedLinks.push({ label: '...', url: null })
+
+        paginatedLinks.push(...leftMidLinks)
+
+        if (pageNo > 2 && pageNo < links.length - 3) paginatedLinks.push(links[pageNo]);
+        if (rightMidLinks.length > 0 && (pageNo + 1 != parseInt(rightMidLinks[0].label))) paginatedLinks.push({ label: '...', url: null })
+
+        paginatedLinks.push(...rightMidLinks)
+
+        if (rightMidLinks.length > 0 && lastLinks.length > 0 && (parseInt(rightMidLinks[rightMidLinks.length - 1].label) + 1 != parseInt(lastLinks[0].label))) paginatedLinks.push({ label: '...', url: null })
+
+        paginatedLinks.push(...lastLinks)
+        paginatedLinks.push(links[links.length - 1])
+
+        return paginatedLinks
+    }
+
+    const displayedLink = formatLinks(links)
+
     return (
         links.length > 3 ?
             <div className="py-6 text-center">
-                {links.map((link, i) => (
-                    link.url ? (
-                        <Link
-                            key={link.label}
-                            href={buildUrl(link.url)}
-                            className={`winston border-2 border-[#424242] px-4 py-2 mx-1 font-bold ${link.active ? 'text-white bg-[#ff5e00]' : (isNaN(link.label) ? 'bg-[#ff5e00] text-white' : 'text-gray-500')}`}
-                        >
-                            {i == 0 ? "«" : i == links.length - 1 ? "»" : link.label}
-                        </Link>
-                    ) : null
+                {displayedLink.map((link, i) => (
+                    link.label != "..." ? (
+                        link.url ? (
+                            <Link
+                                key={i}
+                                href={buildUrl(link.url)}
+                                className={`winston border-2 border-[#424242] px-4 py-2 mx-1 font-bold ${link.active ? 'text-white bg-[#ff5e00]' : (isNaN(link.label) ? 'bg-[#ff5e00] text-white' : 'text-gray-500')}`}
+                            >
+                                {i == 0 ? "«" : i == displayedLink.length - 1 ? "»" : link.label}
+                            </Link>
+                        ) : null
+                    ) : (<span key={i} className="mx-1">{link.label}</span>)
                 ))}
             </div>
             : null
